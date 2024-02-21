3d6d4fff64408dc62abc398c89f8326d235267dd
Harsh Patel <patelharsh9797@users.noreply.github.com>
2024-02-21 11:12:05 +0530

    Indent Scope animation Disabled

--- lua/plugins/indentscope.lua
+++ lua/plugins/indentscope.lua
@@ -1,10 +1,12 @@
 return {
-	"echasnovski/mini.indentscope",
-	opts = {
-		draw = {
-			animation = function()
-				return 0
-			end,
+	{
+		"echasnovski/mini.indentscope",
+		opts = {
+			draw = {
+				animation = function()
+					return 0
+				end,
+			},
 		},
 	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,15 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
+			rainbow = {
+				enable = true,
+				-- list of languages you want to disable the plugin for
+				-- disable = { "jsx", "cpp" },
+				-- Which query to use for finding delimiters
+				-- query = "rainbow-parens",
+				-- Highlight the entire buffer all at once
+				-- strategy = require("ts-rainbow").strategy.global,
+			},
 			ensure_installed = {
 				"css",
 				"gitignore",
